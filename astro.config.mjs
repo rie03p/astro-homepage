002e641d665237ad0d4288b171edf7b04d6a7541
koto74 <kotoganaiyo@gmail.com>
2025-02-16 22:04:29 +0900

    feat: 初期設定ファイルと依存関係を追加

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "src/styles/_variables.scss";`,
+        },
+      },
+    },
+  },
+});
