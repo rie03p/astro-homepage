7055e68ea290e262c8cf90d15a1c42dda334f2c1
koto74 <kotoganaiyo@gmail.com>
2025-02-19 18:54:44 +0900

    feat: SCSSのインポート方法を変更し、マップ機能を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@import "src/styles/_variables.scss";`,
+          additionalData: `@use "src/styles/_variables.scss" as *;`,
         },
       },
     },
